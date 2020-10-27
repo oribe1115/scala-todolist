caab31b5a22865766b40c8b0ebe0efdb87bc8f8b
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-10-27 02:37:13 +0000

    hello系の練習

--- app/controllers/TodoListController.scala
+++ app/controllers/TodoListController.scala
@@ -12,9 +12,21 @@ class TodoListController @Inject()(cc: ControllerComponents) extends AbstractCon
   /**
     * インデックスページを表示
     */
-  def index = Action { implicit request =>
-    // 200 OK ステータスで app/views/index.scala.html をレンダリングする
-    Ok(views.html.index("Welcome to Play application!"))
-  }
+  def index =
+    Action { implicit request =>
+      // 200 OK ステータスで app/views/index.scala.html をレンダリングする
+      Ok(views.html.index("Welcome to Play application!"))
+    }
+
+  def helloWorld =
+    Action { implicit request =>
+      // 200 OK ステータスで app/views/index.scala.html をレンダリングする
+      Ok(views.html.index("Hello world!"))
+    }
+
+  def helloToAny(name: String) =
+    Action { implicit request =>
+      Ok(views.html.index(s"Hello ${name}!"))
+    }
 
 }
